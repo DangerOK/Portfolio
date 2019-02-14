dcee2937de57c6913ef9c63a42ee0280ae3c2f6c
Adam Gawish <gawishadam@gail.com>
2019-02-14 13:15:43 -0500

    add CNAME file and modify package

--- src/components/portfolio/moviesomething/MovieSomething.js
+++ src/components/portfolio/moviesomething/MovieSomething.js
@@ -4,9 +4,9 @@ import MoviesomethingThumb from '../../images/MoviesomethingThumb.png';
 
 const MovieSomething = () => (
   <div className='project'>
-    <a href="https://dangerok.github.io/Movie-Something">
+    <a href="https://dangerok.github.io/Movie-Something" target='_blank' rel="noopener noreferrer">
       <h2>MovieSomething</h2>
-      <img src={MoviesomethingThumb} alt='site thumbnail' />
+      <img src={MoviesomethingThumb} alt='site thumbnail'  />
       <p>Web App using theMovieDB API for searching for movies by rating, genre, realease year, and more.</p>
     </a>
   </div>
